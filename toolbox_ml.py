708c5cdead3cebef2eb66d1dc8a132b3dc8265f2
Nohayla.Ajmi <59204569+nouhin@users.noreply.github.com>
2024-05-08 19:45:55 +0200

    Implementation of logistic regression from scratch

--- toolbox_ml.py
+++ toolbox_ml.py
@@ -1,5 +1,7 @@
 from math import sqrt
 
+import numpy as np
+
 
 class KNN:
     def __init__(self, k):
@@ -37,11 +39,59 @@ class KNN:
         return sum(data) / len(data)
 
 
-if __name__ == '__main__':
-    X = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
-    y = [0, 1, 0, 1, 0]
+class LogisticRegression:
+    """Logistic regression model using gradient descent to optimize the weights and bias."""
+    def __init__(self, lr=0.001, epochs=1000):
+        self.lr = lr
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
 
-    knn = KNN(k=2)
-    knn.fit(X, y)
-    pred = knn.predict(X_pnt=[0, 0])
-    print(pred)  # Output: 0
+    def fit(self, X, y):
+        # Initialize weights and bias
+        n_samples, n_features = len(X), len(X[0])
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.epochs):
+            # Gradient descent
+            self._gradient_descent(X, y, n_samples)
+
+        return self.weights, self.bias
+
+    def predict(self, X):
+        """Predict the class for a given data point."""
+        y_pred = self._predict(X)
+        y_label = [1 if v > 0.5 else 0 for v in y_pred]
+        return y_label
+
+    def _predict(self, X):
+        """Calculate the linear model and apply the sigmoid function."""
+        linear_model = np.dot(X, self.weights) + self.bias
+        return self._sigmoid(linear_model)
+
+    def _sigmoid(self, z):
+        """Sigmoid function."""
+        return 1 / (1 + np.exp(-z))
+
+    def _gradient_descent(self, X, y, n_samples):
+        # Evaluate the loss
+        error_array = y - self._predict(X)  # y is flattened
+        # Calculate the gradients
+        dw = np.dot(X.T, error_array) / n_samples
+        db = np.sum(error_array) / n_samples
+        # Update the weights and bias
+        self.weights += self.lr * dw
+        self.bias += self.lr * db
+
+    def _accuracy(self, y_true, y_pred):
+        return np.sum(y_true == y_pred) / len(y_true)
+
+
+if __name__ == '__main__':
+    X = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]])
+    y = np.array([0, 0, 1, 1, 1])
+    X_test = np.array([[2, 3]])
+    lr = LogisticRegression(lr=0.001, epochs=10000)
+    w, b = lr.fit(X, y)
+    print(lr.predict(X_test))
